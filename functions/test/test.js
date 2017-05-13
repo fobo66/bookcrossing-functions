7c82d934cc8619afef40385e303694d7d4d556d2
Andrey Mukamolow <fobo66@protonmail.com>
2017-05-14 01:01:00 +0300

    Test improved

--- functions/test/test.js
+++ functions/test/test.js
@@ -18,16 +18,47 @@ describe('Retrieving book location', () => {
     adminInitStub = sinon.stub(admin, 'initializeApp');
     functions = require('firebase-functions');
     configStub = sinon.stub(functions, 'config').returns({
-        firebase: {
-            databaseURL: 'https://not-a-project.firebaseio.com',
-            storageBucket: 'not-a-project.appspot.com',
-        },
+      firebase: {
+        databaseURL: 'https://not-a-project.firebaseio.com',
+        storageBucket: 'not-a-project.appspot.com',
+      },
     });
-        myFunctions = require('../index');
+    myFunctions = require('../index');
   });
 
   after(() => {
     configStub.restore();
     adminInitStub.restore();
+  });
+
+  describe('resolveBookLocation', () => {
+    it('should create new reference for every change of the location', () => {
+      const fakeEvent = {
+        data: new functions.database.DeltaSnapshot(null, null, null, {
+          position: 'Grand St/Bedford Av, Brooklyn, NY 11211, США',
+        }),
+        params: {
+          bookKey: 'book',
+        },
+      };
+
+      const refStub = sinon.stub();
+      const childStub = sinon.stub();
+      const setStub = sinon.stub();
+      const setArg = {
+        lat: 40.714224,
+        lng: -73.961452,
+      };
+
+      const databaseStub = sinon.stub(admin, 'database');
+
+      setStub.withArgs(setArg).returns(true);
+      childStub.withArgs('Grand St/Bedford Av, Brooklyn, NY 11211, США').returns({ set: setStub });
+      refStub.withArgs('/places/book').returns({ child: childStub });
+      Object.defineProperty(fakeEvent.data, 'ref', { get: refStub });
+      databaseStub.returns({ ref: refStub });
+
+      return assert.eventually.equal(myFunctions.resolveBookLocation(fakeEvent), true);
+    });
   });
 });
